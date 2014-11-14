16ca478110f0d4bb7092b8a77e977ffed5bab455
huzelin <huzelin1008@163.com>
2014-11-14 17:57:35 +0800

    test query

--- test/emdb_test_trie_search.c
+++ test/emdb_test_trie_search.c
@@ -21,9 +21,52 @@ TestSearch()
    printf("value = %d key=%d\n", value, emdb_trie_num_keys(&trie));
 }
 
+void
+TestSearchHuge()
+{
+   emdb_trie_t trie;
+   emdb_trie_init(&trie);
+#define N 100000
+#define L 20
+   unsigned char** buf_array = (unsigned char**)malloc(sizeof(char*)*N); 
+   int64_t* value   = (int64_t*)malloc(sizeof(int64_t)*N);
+   int i;
+   for(i = 0; i < N; ++i){
+     buf_array[i] = (char*)malloc(sizeof(char)*L);
+     memset(buf_array[i], 0, L);
+     value[i]     = 0;
+   } 
+
+   for(i = 1; i < N; ++i){
+     int j = 0;
+     memcpy(buf_array[i], buf_array[i-1], L);
+     do{
+       buf_array[i][j]++; 
+       if(buf_array[i][j] == 255){
+         buf_array[i][j] = 0;
+         ++j;
+       }else
+         break;
+     }while(j < L);
+     value[i] = i;
+   }
+
+   for(i = 0; i < N; ++i){
+     emdb_trie_update(&trie, buf_array[i], L, value[i]);
+   }
+   printf("after huge search key=%d\n", emdb_trie_num_keys(&trie));
+
+   for(i = 0; i < N; ++i){
+     int64_t v = emdb_trie_exact_match_search(&trie, buf_array[i], L);
+     assert(v == value[i]);
+   }
+   printf("Huge Search Success\n");
+}
+
 int 
 main(int argc, char** argv)
 {
   TestSearch();
+  TestSearchHuge();
   return 1;
 }
