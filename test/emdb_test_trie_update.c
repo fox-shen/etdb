5d8f8b3ba70d97d80eaa260ee912531d7a5ed8f0
huzelin <huzelin1008@163.com>
2014-11-14 10:09:42 +0800

    test src seperation

--- test/emdb_test_trie_update.c
+++ test/emdb_test_trie_update.c
@@ -7,15 +7,17 @@ TestUpdate()
    emdb_trie_t trie;
    emdb_trie_init(&trie);
    unsigned char buf[100];
-   int loop = 300, idx;
+   int loop = 900000, idx;
    for(idx = 0; idx < loop; ++idx){
      int i;
      for(i = 0; i < 100; ++i){
        buf[i] = (random())%256;
+       if(buf[i] == 0) buf[i] = 'a';
      }
-     emdb_trie_update(&trie, buf, 10, idx);
+     emdb_trie_update(&trie, buf, 100, idx);
    }
    printf("TestUpdate Time: %d(ms)/%d updates\n", (emdb_utls_get_timestamp() - time_stamp_1)/1000, loop);
+   printf("size: %d nonzero-size: %d\n", emdb_trie_total_size(&trie), emdb_trie_nonzero_size(&trie));
 }
 
 int 
