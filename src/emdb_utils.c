554228c5537eaf4ce189620d9da52ecd39487a4b
huzelin <huzelin1008@163.com>
2014-11-15 00:53:26 +0800

    del

--- src/emdb_utils.c
+++
@@ -1,9 +0,0 @@
-#include <emdb.h>
-
-long int 
-emdb_utls_get_timestamp()
-{
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec*1000000 + tv.tv_usec;
-}
--- src/emdb_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef H_EMDB_UTILS_H
-#define H_EMDB_UTILS_H
-
-long int emdb_utls_get_timestamp();
-
-#endif
