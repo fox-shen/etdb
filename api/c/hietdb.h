17b3d58cd8071f053364f196b7a91564bc11e687
huzelin <huzelin1008@163.com>
2014-11-30 18:27:17 +0800

    update

--- api/c/hietdb.h
+++ api/c/hietdb.h
@@ -34,8 +34,9 @@ typedef etdb_str_t etdb_status_t;
 int etdb_client_connect(etdb_client_t *client, const char *host, int port);
 
 /**** etdb api *****/
+/*** db: type k-v ***/
 etdb_status_t etdb_set(etdb_client_t *client, etdb_str_t *key, etdb_str_t *value);
 etdb_status_t etdb_get(etdb_client_t *client, etdb_str_t *key, etdb_str_t *value);
-
+etdb_status_t etdb_del(etdb_client_t *client, etdb_str_t *key);
 
 #endif
